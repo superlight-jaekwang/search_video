e631a9d42cc4c91d4ff6c5f93a2f4b43d032fbbb
김재광 <hargreajk@gmail.com>
2019-05-03 10:50:29 +0900

    update

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import _ from 'lodash'
+import YTSearch from 'youtube-api-search'
+
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+
+const API_KEY = 'AIzaSyCR7QfZRkcygxcIUkLCsTpmU1_d6APGlMk'
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        }
+
+        this.videoSearch('Tottenham')
+    }
+
+    videoSearch = term => {
+        YTSearch({key: API_KEY, term}, videos => {
+            this.setState({
+                videos,
+                selectedVideo: videos[0]
+            })
+        })
+    }
+
+    render() {
+        const videoSearch = _.debounce(term => {this.videoSearch(term)}, 300)
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}
+                />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
